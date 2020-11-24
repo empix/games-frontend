991c6c9c1e7e4e77c80bb38332559cbb592af11a
Empix <jotinhave@gmail.com>
2020-11-24 19:33:13 -0400

    Consume games API

--- src/pages/GameList/index.js
+++ src/pages/GameList/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import './style.css';
 
@@ -7,56 +7,60 @@ import { FiTrash2, FiEdit } from 'react-icons/fi';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
+import api from '../../services/api';
+
 export default function ListGames() {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    api.get('games').then((response) => {
+      setGames(response.data);
+    });
+  }, []);
+
   return (
     <div className="container">
       <Header />
       <section className="list-container">
-        {(() => {
-          let games = [];
-
-          for (let i = 0; i < 10; i++) {
-            games.push(
-              <div className="game" key={i}>
-                <h1 className="title">Undertale</h1>
-                <div className="details">
-                  <p>
-                    <strong>Release Date:</strong> 15/09/2015
-                  </p>
-                  <p>
-                    <strong>Genres:</strong> Adventure, Indie, Role-playing
-                    (RPG), Shooter, Turn-based strategy (TBS)
-                  </p>
-                  <p>
-                    <strong>Developer</strong> tobyfox
-                  </p>
-                  <p>
-                    <strong>Publisher</strong> tobyfox
-                  </p>
-                </div>
-                <p className="description">
-                  <strong>Description:</strong> A small child falls into the
-                  Underground, where monsters have long been banished by humans
-                  and are hunting every human that they find. The player
-                  controls the child as they try to make it back to the Surface
-                  through hostile environments, all the while engaging with a
-                  turn-based combat system with puzzle-solving and bullet hell
-                  elements, as well as other unconventional game mechanics.
-                </p>
-                <div className="actions">
-                  <button type="button">
-                    <FiTrash2 size={20} color="#eee" />
-                  </button>
-                  <button type="button">
-                    <FiEdit size={20} color="#eee" />
-                  </button>
-                </div>
-              </div>
-            );
-          }
-
-          return games;
-        })()}
+        {games.map((game) => (
+          <div className="game">
+            <h1 className="title">{game.name}</h1>
+            <div className="details">
+              <p>
+                <strong>Release Date:</strong> {game.release_date}
+              </p>
+              <p>
+                <strong>Genres:</strong>{' '}
+                {game.genres.map((genre) => genre.name + ', ')}
+              </p>
+              <p>
+                <strong>Engine:</strong> {game.engine.name}
+              </p>
+              <p>
+                <strong>Developers: </strong>{' '}
+                {game.developers.map((developer) => developer.name + ', ')}
+              </p>
+              <p>
+                <strong>Publishers: </strong>{' '}
+                {game.publishers.map((publisher) => publisher.name + ', ')}
+              </p>
+            </div>
+            <p className="description">
+              <strong>Description:</strong> {game.description}
+            </p>
+            <div className="actions">
+              <button type="button">
+                <FiTrash2 size={20} color="#eee" />
+              </button>
+              <button type="button">
+                <FiEdit size={20} color="#eee" />
+              </button>
+            </div>
+          </div>
+        ))}
+        {games.length === 0 && (
+          <h1 className="not-found">Não foi encontrado nenhum jogo :(</h1>
+        )}
       </section>
       <Footer />
     </div>
