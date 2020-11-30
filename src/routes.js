1ce1ce3d2292b10454b423f887f9b88a469448d0
Empix <jotinhave@gmail.com>
2020-11-30 19:43:51 -0400

    Create route to AddGame page

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import GameList from './pages/GameList';
+import AddGame from './pages/AddGame';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={GameList} />
+        <Route path="/add" component={AddGame} />
       </Switch>
     </BrowserRouter>
   );
