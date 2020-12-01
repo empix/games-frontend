8a16306ca3afad1fdfa07d335e09b339ed20ac0e
Empix <jotinhave@gmail.com>
2020-12-01 15:34:24 -0400

    Create route for ListData page

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import GameList from './pages/GameList';
 import AddGame from './pages/AddGame';
+import ListData from './pages/ListData';
 
 export default function Routes() {
   return (
@@ -10,6 +11,7 @@ export default function Routes() {
       <Switch>
         <Route path="/" exact component={GameList} />
         <Route path="/add" component={AddGame} />
+        <Route path="/data" component={ListData} />
       </Switch>
     </BrowserRouter>
   );
